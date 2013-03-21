4e974c408276a904364b48dccc9ffb20c7155356
Paul <paul@pogodan.com>
2013-03-20 22:11:01 -0400

    updates for running with ENV-specified port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var signaling = require('./server/lib/signalingServer.js');
 var router = require('./server/lib/router.js')
 var server;
+var port = process.env.PORT || '13337';
 
 var allowCrossDomain = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -15,15 +16,15 @@ app.use(express.static(__dirname + '/public'));
 
 app.configure('development', function () {
     app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
-    console.log('listening to port 13337')
-    server = app.listen(13337);
+    console.log('listening to port ' + port)
+    server = app.listen(port);
     signaling.start(server);
     console.log('here I am');
 });
 
 app.configure('production', function () {
-    console.log('listening to port 80')
-    server = app.listen(80); //nodejitsu will map this to 80
+    console.log('listening to port ' + port)
+    server = app.listen(port); //nodejitsu will map this to 80
     signaling.start(server);
 });
 
